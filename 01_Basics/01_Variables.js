c457afe95b64c34181cf5ae54ca8a5d9d81220d7
Dikshasinha178 <Sinhadiksha2017@gmail.com>
2025-04-05 07:23:25 +0000

    Container config done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,10 @@
+console.log("Diksha");
+const accountID = 144325
+let accountemail ="sinhadiksha2017.com"
+Var account password = "12345"
+accountcity = "jaipur"
+
+
+accountemail = "hc@hc.com"
+account password = "1235"
+console.table([accountemail,account password]);
